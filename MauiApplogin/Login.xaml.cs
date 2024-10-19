925b87daac59985d9c41145b194063c78fe32c22
EDUARDO SILVA <eduardo.silva1305@etec.sp.gov.br>
2024-10-19 00:47:56 -0300

    ADD

--- MauiApplogin/Login.xaml.cs
+++ MauiApplogin/Login.xaml.cs
@@ -6,4 +6,15 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+
+		}catch (Exception ex) 
+		{
+			DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+	}
+
 }
